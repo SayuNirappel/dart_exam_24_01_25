30f7c7d95e4364a98a26ead955bbc37375dafaad
SayuNirappel <sayunirappel@gmail.com>
2025-01-23 14:02:29 +0530

    first commit

---
+++ bin/dart_exam_24_01_25.dart
@@ -0,0 +1,6 @@
+import 'package:dart_exam_24_01_25/dart_exam_24_01_25.dart' as dart_exam_24_01_25;
+
+void main()
+{
+
+}
---
+++ test/dart_exam_24_01_25_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_exam_24_01_25/dart_exam_24_01_25.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
